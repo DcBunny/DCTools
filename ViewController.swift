15a2db2066a7877d96cefdb640040310f45687fd
DcBunny <515665517@qq.com>
2016-05-19 16:40:15 +0800

    更新tips

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,49 @@ class ViewController: UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+    }
+}
+
+//MARK: - 自省，判断对象属于的类型
+
+/*
+ *  自省，判断对象属于的类型
+ */
+
+// 只能对Class生效
+func whoAmI1() {
+    class ClassA { }
+    class ClassB: ClassA { }
+    
+    let obj1: AnyObject = ClassB()
+    let obj2: AnyObject = ClassB()
+    
+    if obj1.isKindOfClass(ClassA.self) {
+        print("true")
+    } else {
+        print("false")
+    }
+    
+    if obj2.isMemberOfClass(ClassA.self) {
+        print("true")
+    } else {
+        print("false")
+    }
+}
+
+// 除了用于Class，还可以用于struct和enum
+func whoAmI2() {
+    class ClassA { }
+    class ClassB: ClassA { }
+    
+    let obj: AnyObject = ClassB()
+    
+    if (obj is ClassA) {
+        print("属于 ClassA")
+    }
+    
+    if (obj is ClassB) {
+        print("属于 ClassB")
     }
 }
 
